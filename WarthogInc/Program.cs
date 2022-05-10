75e9ad72ecdd8615d00ce0ded438d9f7a8c44803
Codie Stella <git@codie.gg>
2022-05-10 20:54:17 +0100

    Hopper descriptions.

--- WarthogInc/Program.cs
+++ WarthogInc/Program.cs
@@ -11,73 +11,8 @@ namespace WarthogInc
     {
         static void Main(string[] args)
         {
-            //BitStream<StreamByteStream> hoppersIn = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("C:\\Users\\codie\\Desktop\\hoppers", FileMode.Open)));
-
-            //HopperConfigurationTable hoppers = new HopperReader().ReadHoppers(hoppersIn);
-
-            //hoppers.categories[2].identifier = 10;
-            //hoppers.categories[2].name = "Sunrise";
-            //hoppers.categories[2].image = hoppers.categories[3].image;
-
-            //hoppers.categories[3].identifier = 11;
-            //hoppers.categories[3].name = "Sunrise Launch";
-            //hoppers.categories[3].image += 1;
-
-            //ulong startTime = 0;
-            //ulong endTime = 0;
-
-            //hoppers.configurations[15].startTime = startTime;
-            //hoppers.configurations[16].startTime = startTime;
-            //hoppers.configurations[22].startTime = startTime;
-            //hoppers.configurations[24].startTime = startTime;
-
-            //hoppers.configurations[15].endTime = endTime;
-            //hoppers.configurations[16].endTime = endTime;
-            //hoppers.configurations[22].endTime = endTime;
-            //hoppers.configurations[24].endTime = endTime;
-
-            //hoppers.configurations[16].require_all_party_members_meet_live_account_access_requirements = false;
-            //hoppers.configurations[22].require_all_party_members_meet_live_account_access_requirements = false;
-            //hoppers.configurations[24].require_all_party_members_meet_live_account_access_requirements = false;
-
-            //hoppers.configurations[15].name = "Quick Match";
-            //hoppers.configurations[15].minimum_player_count = 1;
-            //hoppers.configurations[15].maximum_player_count = 15;
-            //hoppers.configurations[15].type = 0;
-            //hoppers.configurations[15].maximumPartySize = 15;
-            //hoppers.configurations[15].identifier = 200;
-            //hoppers.configurations[15].hashSet = hoppers.configurations[6].hashSet;
-            //hoppers.configurations[15].guests_allowed = true;
-            //hoppers.configurations[15].category = 10;
-            //hoppers.configurations[15].xLastIndex = 200;
-            //hoppers.configurations[15].richPresenceId = 78;
-            //hoppers.configurations[15].experience_points_per_win = 1;
-
-            //BitStream <StreamByteStream> hoppersOut = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("C:\\Users\\codie\\Desktop\\hoppers_out", FileMode.OpenOrCreate)));
-            //new HopperReader().WriteHoppers(hoppersOut, hoppers);
-
-            var streamHelper = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("C:\\Users\\codie\\Desktop\\descriptions", FileMode.Open)));
-            HopperDescriptions descriptions = new HopperReader().ReadHopperDescriptions(streamHelper);
-
-            descriptions.descriptions[27].description = "Sunrise Launch|r|nWelcome back to matchmaking, we're throwing a launch party!|r|nWinners receive double EXP points.      ";
-            descriptions.descriptions[27].identifier = 11;
-
-            Array.Resize(ref descriptions.descriptions, 29);
-
-            descriptions.descriptionCount = 29;
-            descriptions.descriptions[28] = new HopperDescriptions.HopperDescription();
-            descriptions.descriptions[28].description = "Sunrise|r|nCelebrate the revival of Halo 3 with all new playlists!";
-            descriptions.descriptions[28].identifier = 10;
-
-
-            descriptions.descriptions[15].description = "<color argb=#FF8692d4>In a rush? You've got time for one more game right?|r|nFind some players and give em hell!</color>|r|nMax Party Size: 16";
-            descriptions.descriptions[15].identifier = 200;
-
-            BitStream<StreamByteStream> descriptionsOut = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("C:\\Users\\codie\\Desktop\\descriptions_out", FileMode.OpenOrCreate)));
-            new HopperReader().WriteHopperDescriptions(descriptionsOut, descriptions);
-
             File.Delete("C:\\Users\\codie\\Desktop\\user.bin");
-            streamHelper = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("C:\\Users\\codie\\Desktop\\user.bin", FileMode.CreateNew)));
+            var streamHelper = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("C:\\Users\\codie\\Desktop\\user.bin", FileMode.CreateNew)));
 
             _blf blfFileHeader = new _blf();
             ServiceRecordIdentity srid = new ServiceRecordIdentity()
@@ -106,6 +41,7 @@ namespace WarthogInc
                 hopperDirectory = "default_hoppers"
             };
 
+            // This chunk should be refactored into a "BLF File Writer" which writes an array of chunks.
             BLFChunkWriter bLFChunkWriter = new BLFChunkWriter();
             bLFChunkWriter.WriteChunk(ref streamHelper, blfFileHeader);
 
@@ -119,7 +55,7 @@ namespace WarthogInc
 
             //hoppersIn.Seek(0);
             //File.Delete("C:\\Users\\codie\\Desktop\\matchmaking_hopper_011.bin");
-            var hoppersOut = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("../../../../blf/matchmaking_hopper_011.bin", FileMode.CreateNew)));
+            var hoppersOut = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("../../../../blf/matchmaking_hopper_011.bin", FileMode.Create)));
             //mhcf.configurations[4].name = "Hello, World!";
             bLFChunkWriter.WriteChunk(ref hoppersOut, blfFileHeader);
             bLFChunkWriter.WriteChunk(ref hoppersOut, mhcf);
@@ -129,8 +65,26 @@ namespace WarthogInc
             var setting = new JsonSerializerSettings { Converters = { new ByteArrayConverter(), new HexStringConverter() }, Formatting = Formatting.Indented };
             string output = JsonConvert.SerializeObject(mhcf, setting);
 
-            File.WriteAllText("./out/matchmaking_hopper_011.json", output);
+            //File.WriteAllText("./out/matchmaking_hopper_011.json", output);
             //streamHelper.Write(0, 8);
+
+            var mhdf = JsonConvert.DeserializeObject<Sunrise.BlfTool.HopperDescriptions>(File.ReadAllText("../../../../json/en/matchmaking_hopper_descriptions_003.json"));
+            //var mhdf = new HopperReader().ReadNewHopperDescriptions(new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("C:\\Users\\codie\\Desktop\\descriptions", FileMode.Open))));
+
+
+            var descriptionsOut = new BitStream<StreamByteStream>(new StreamByteStream(new FileStream("../../../../blf/en/matchmaking_hopper_descriptions_03.bin", FileMode.Create)));
+            bLFChunkWriter.WriteChunk(ref descriptionsOut, blfFileHeader);
+            bLFChunkWriter.WriteChunk(ref descriptionsOut, mhdf);
+            bLFChunkWriter.WriteChunk(ref descriptionsOut, new _eof(blfFileHeader.GetLength() + mhdf.GetLength() + (0xC * 2)));
+            descriptionsOut.Write(0, 8);
+
+
+            //output = JsonConvert.SerializeObject(mhdf, setting);
+            //File.WriteAllText("../../../../json/en/matchmaking_hopper_descriptions_003.json", output);
+
+
+
+
         }
     }
 }
